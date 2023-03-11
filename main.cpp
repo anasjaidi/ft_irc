cf3391cb3520dbff9c1719def54e71d01c84ed50
mustapha ouarsass <hzan1996@gmail.com>
2023-03-11 01:20:26 +0100

    added the main.cpp

---
+++ main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+
+int main(int argc, char *argv[])
+{
+    if (argc != 3)
+    {
+        std::cout << "ERROR :\nthe argument is not correct please you have to enter lik this :" << std::endl;
+        std::cout << "./ircserv <port> <password>" << std::endl;
+        exit(EXIT_FAILURE);
+    }
+
+    std::cout << "done\n";
+    return (0);
+}
