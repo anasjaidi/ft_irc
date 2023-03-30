b7d2f6d567adcab7245b0a2224f3f21ac06ee3c9
Anas jaidi <anas.jaidi@icloud.com>
2023-03-30 11:48:08 +0000

    add client and client manager

--- models/client/client.hpp
+++ models/client/client.hpp
@@ -4,10 +4,12 @@
 
 #ifndef FT_IRC_CLIENT_HPP
 #define FT_IRC_CLIENT_HPP
-
+#include "../../FT_IRC.h"
 
 class client {
-
+private:
+    std::string user;
+    std::string nick;
 };
 
 
---
+++ models/client/client_manager/client_manager.hpp
@@ -0,0 +1,17 @@
+//
+// Created by anas jaidi on 30/3/2023.
+//
+
+#ifndef FT_IRC_CLIENT_MANAGER_HPP
+#define FT_IRC_CLIENT_MANAGER_HPP
+
+# include "../../../FT_IRC.h"
+# include "../client.hpp"
+
+class client_manager {
+protected:
+    std::vector<client> clients;
+};
+
+
+#endif //FT_IRC_CLIENT_MANAGER_HPP
