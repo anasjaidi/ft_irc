ef2c2c6d6c3f8c64b495a4296161db74c18e426b
Zakaria Maziane <ziko98greenois@gmail.com>
2023-04-15 01:36:10 +0000

    fix parssing client exist, kick fin commented

--- server/irc/commands/cmd/kickCmd.cpp
+++ server/irc/commands/cmd/kickCmd.cpp
@@ -4,33 +4,54 @@
 #include "../Commands.hpp"
 
 void Commands::kick(std::string payload, int client_fd) {
-
+//payload = #ch|med
+//
     int fdcl = -1;
     std::string msg;
     std::vector<std::string> cmd = split(payload, '|');
-    std::vector<std::string> users = split(cmd[1], '$');
+
+    //set iterators of channels,clients
+
     std::vector<channel>::iterator itChan = channels.begin();
     std::vector<client>::iterator itclient = clients.begin();
+
+    // check error parsing
+
+    if(payload == "No such nick/channel"){
+        msg = "-> :localhost 401 " + cmd[1] + " : No such nick/channel\r\n";
+        send(client_fd, msg.c_str(),msg.size(),0);
+        return;
+    }
+
+    // loop to find the current channel
+
     for (; itChan != channels.end(); itChan++) {
 
         std::string name = itChan->getName();
         trim_fun(name);
 
+        // check name of channel
+
         if(name == cmd[0]){
             std::string clName = itclient->getNick();
             trim_fun(clName);
             int i= 0;
-            while(i < users.size()){
+
+            //check nick client ,check is the current one and get his file descriptor
+            if(!cmd[1].empty()){
                 for(; itclient != clients.end(); itclient++) {
-                    if(clName == users[i]){
+                    if(clName == cmd[1]){
                         fdcl = itclient->getFd();
+
+                        // check if kicker is operator
+
                         if(this->isOperator(client_fd ,itChan)){
                             if(itChan->itIsInChannel(fdcl)){
                                 itChan->delete_client(fdcl, 'k');
-                                msg = users[i] + "is kicked\r\n";
+                                msg = cmd[1] + "is kicked\r\n";
                                 send(client_fd, msg.c_str(), msg.size(), 0);
                             }
-                            msg = ":localhost 441 " + users[i]  + " :isn't on that channel\r\n";
+                            msg = ":localhost 441 " + cmd[1]  + " :isn't on that channel\r\n";
                             send(client_fd, msg.c_str(),msg.size(), 0);
                             return;
                         }
@@ -38,12 +59,10 @@ void Commands::kick(std::string payload, int client_fd) {
                         send(client_fd, msg.c_str(),msg.size(), 0);
                         return;
                     }
-                        std::cout << "fdClient :" << fdcl;
-                    }
                 }
-                i++;
             }
-        msg = "-> :localhost 401 " + users[0] + " : No such nick/channel\r\n";
+        }
+        msg = "-> :localhost 401 " + cmd[1] + " : No such nick/channel\r\n";
         send(client_fd,msg.c_str(),msg.size(),0);
         }
     }
@@ -58,16 +77,32 @@ void Commands::kick(std::string payload, int client_fd) {
 
 
 std::string Commands::parse_kick_command(std::string &req) {
+    bool Nk = false;
      std::string payload;
      std::vector<std::string> cmd = split(req, ' ');
      std::string channelName = cmd[1];
-     std::vector<std::string> users  = split(cmd[2], ',');
-     if (cmd.size() != 3 && channelName[0] != '#')
-         return  "Error"; // rpl = ":localhost 401 " + cmd[1] + " : No such nick/channel\r\n";
+    trim_fun(channelName);
+//     std::vector<std::string> users  = split(cmd[2], ',');
+     std::string user = cmd[2];
+    trim_fun(user);
+    std::vector<client>::iterator itclient = clients.begin();
 
+     if (cmd.size() != 3 && channelName[0] != '#')
+         return  "No such nick/channel"; // rpl = ":localhost 401 " + cmd[1] + " : No such nick/channel\r\n";
+     else if(!user.empty()){
+         for(;itclient != clients.end(); itclient++){
+             std::string clNick = itclient->getNick();
+             trim_fun(clNick);
+             if(user == clNick)
+                 Nk = true;
+         }
+         if(Nk == false)
+             return"No such nick/channel"; // rpl = ":localhost 401 " + cmd[1] + " : No such nick/channel\r\n";
+
+         }
      else{
         payload = channelName + '|';
-        payload += joinByMe(users, '$');
+        payload += user;
      }
      std::cout << payload <<std::endl;
     return (payload);
