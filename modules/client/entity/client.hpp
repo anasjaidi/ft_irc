bbb6150550ad1371c3977c29244ffca7d311bd29
anas jaidi <anas.jaidi@icloud.com>
2023-05-20 16:45:34 +0100

    add client entity

---
+++ modules/client/entity/client.hpp
@@ -0,0 +1,51 @@
+//
+// Created by anas jaidi on 30/3/2023.
+//
+
+#ifndef FT_IRC_CLIENT_HPP
+#define FT_IRC_CLIENT_HPP
+
+#include "../../../FT_IRC.h"
+
+class client
+{
+private:
+  std::string user;
+  std::string nick;
+  int fd;
+
+  bool isAuth;
+  struct sockaddr_storage their_addr;
+
+public:
+  bool isAuth1() const;
+
+  void setIsAuth(bool isAuth);
+  int getFd() const;
+
+  void setFd(int fd);
+
+  const sockaddr_storage &getTheirAddr() const;
+
+  void setTheirAddr(const sockaddr_storage &theirAddr);
+
+  client(int fd, const sockaddr_storage &theirAddr);
+
+public:
+  const std::string &getUser() const;
+
+  void setUser(const std::string &user);
+
+  const std::string &getNick() const;
+
+  void setNick(const std::string &nick);
+
+  client(const std::string &user, const std::string &nick);
+
+public:
+  virtual ~client();
+
+  client();
+};
+
+#endif // FT_IRC_CLIENT_HPP
