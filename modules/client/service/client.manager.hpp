28dbdb031bc45725c41684f767a8c35746f72709
anas jaidi <anas.jaidi@icloud.com>
2023-05-20 16:49:50 +0100

    finish client services manager

---
+++ modules/client/service/client.manager.hpp
@@ -0,0 +1,30 @@
+#ifndef FT_IRC_CLIENT_MANAGER_HPP
+#define FT_IRC_CLIENT_MANAGER_HPP
+
+#include "../../../FT_IRC.h"
+#include "../entity/client.hpp"
+class client_manager
+{
+protected:
+  std::vector<client> clients;
+
+public:
+  std::vector<client>::iterator get_client(int ID);
+
+  int remove_client(int ID);
+
+  int add_client(int fd, struct sockaddr_storage infos);
+
+  int get_client_id(int fd);
+  int get_client_id_by_nick(std::string &);
+
+  enum update_action
+  {
+    UpdateNick,
+    UpdateUserName,
+  };
+
+  int update_client_info(update_action action, std::string &payload, int ID);
+};
+
+#endif // FT_IRC_CLIENT_MANAGER_HPP
